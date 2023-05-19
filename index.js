bea981c7bc2aa5ed90ed6a3ef0fb85ff3da0c8d4
Tai <Taiho051493@yahoo.com>
2023-05-18 22:45:42 -0500

    final commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadme = require("./utils/generateReadme")
+const generateReadme = require("./generateReadme.js");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser(){
